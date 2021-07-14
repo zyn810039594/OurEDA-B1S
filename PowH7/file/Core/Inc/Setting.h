6c31e304383da8a5ca017cb681e8b48c89cb9fee
Redlight <dddbbbdd@foxmail.com>
2021-07-14 14:47:05 +0800

    Update USART

--- PowH7/file/Core/Inc/Setting.h
+++ PowH7/file/Core/Inc/Setting.h
@@ -6,7 +6,7 @@
 #define PowerSide
 
 //数据校验开关
-//#define DataIdentify
+#define DataIdentify
 
 //PID数据位
 //定深PID参数
@@ -15,7 +15,7 @@
 //定深积分系数
 #define PID_D_Ki 2
 //定深微分系数
-#define PID_D_Kd 300
+#define PID_D_Kd 0
 
 //定向PID参数
 //定向比例系数
@@ -23,6 +23,6 @@
 //定向积分系数
 #define PID_O_Ki 1
 //定向微分系数
-#define PID_O_Kd 150
+#define PID_O_Kd 0
 
 #endif
